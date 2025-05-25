b53a9edfb96622f860bdf72dad80fb8cf1fe8d67
gtpgame37 <gtpgame37@gmail.com>
2025-05-25 11:18:42 +0800

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,39 @@
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Position move(int dx, int dy) {
+        return new Position(x + dx, y + dy);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Position)) return false;
+        Position other = (Position) obj;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
